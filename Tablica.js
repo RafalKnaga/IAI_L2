8607dbb3ba4aa038dfeeb93e26cec7ea9276853c
RafalKnaga <30686@student.pwsztar.edu.p>
2018-11-19 00:42:04 +0100

    zmiany

---
+++ Tablica.js
@@ -0,0 +1,12 @@
+//Zadanie 1
+console.log("Hello World!");
+
+//Zadanie 2
+
+for(let i=1; i<=10; i++){   
+    let array=[];
+    for(let j=1; j<=10; j++){
+        array.push(i*j);
+   }
+    console.log(array);
+}
