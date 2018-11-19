fe3d0275d9e962ad4984858a369e8ec66d442602
Dawid <troyan12312@gmail.com>
2018-11-19 18:08:04 +0100

    navbar

---
+++ js/navbar.js
@@ -0,0 +1,17 @@
+
+let nav = '<nav>\n' +
+
+   ' <div id="header">' +
+    '          <ul>\n' +
+    '      <li><a href="index.html">Start</a></li>\n ' +
+    '      <li><a href="kontakt.html">Kontakt</a></li>\n' +
+    '      <li><a href="map.html">Mapa</a></li>\n'  +
+    '          </ul>\n' +
+    '   </div>\n ' +
+    '   </nav>';
+
+
+
+let navBar = document.querySelector ('.navbar');
+
+navBar.innerHTML = nav;
